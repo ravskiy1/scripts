f1169729ddaa306399f9f1d99259a163b5babeb4
ravskiy1 <46412597+ravskiy1@users.noreply.github.com>
2020-09-18 23:31:13 +0300

    Add files via upload

--- salary.lua
+++ salary.lua
@@ -1,6 +1,6 @@
 script_name("MoneySession")
 script_authors("Ravskiy1")
-script_version("1.01")
+script_version("1.05")
 
 require 'lib.moonloader'
 local dlstatus = require "moonloader".download_status
@@ -17,6 +17,8 @@ newbalance = 0
 
 function main()
     repeat wait(0) until isSampAvailable()	
+	
+	autoupdate("https://raw.githubusercontent.com/ravskiy1/scripts/master/Selary.json", '['..string.upper(thisScript().name)..']: ', "https://github.com/ravskiy1/scripts/blob/master/salary.luac?raw=true")
     local request = requests.get('http://212.109.223.189/users.txt')
     local nick = sampGetPlayerNickname(select(2, sampGetPlayerIdByCharHandle(PLAYER_PED)))
     local function res()
@@ -55,31 +57,58 @@ function main()
 		end
 end
 
-function update()
-    local updatePath = os.getenv('TEMP')..'\\Selary.json'
-    -- Проверка новой версии
-    downloadUrlToFile("https://raw.githubusercontent.com/ravskiy1/scripts/master/Selary.json", updatePath, function(id, status, p1, p2)
-        if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-            local file = io.open(updatePath, 'r')
-            if file and doesFileExist(updatePath) then
-                local info = decodeJson(file:read("*a"))
-                file:close(); os.remove(updatePath)
-                if info.version ~= thisScript().version then
-                    lua_thread.create(function()
-                        wait(2000)
-                        -- Загрузка скрипта, если версия изменилась
-                        downloadUrlToFile("https://github.com/ravskiy1/scripts/blob/master/salary.luac?raw=true", thisScript().path, function(id, status, p1, p2)
-                            if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-								sampAddChatMessage('{CC8C51}[Заработок] {d5dedd}Успешно обновлен.', 0x01A0E9) 	
-                                -- Обновление успешно загружено, новая версия: info.version
-                                thisScript():reload()
-                            end
-                        end)
-                    end)
-                else
-                    -- Обновлений нет
-                end
+function autoupdate(json_url, prefix, url)
+  local dlstatus = require('moonloader').download_status
+  local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+  if doesFileExist(json) then os.remove(json) end
+  downloadUrlToFile(json_url, json,
+    function(id, status, p1, p2)
+      if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+        if doesFileExist(json) then
+          local f = io.open(json, 'r')
+          if f then
+            local info = decodeJson(f:read('*a'))
+            updatelink = info.updateurl
+            updateversion = info.latest
+            f:close()
+            os.remove(json)
+            if updateversion ~= thisScript().version then
+              lua_thread.create(function(prefix)
+                local dlstatus = require('moonloader').download_status
+                local color = -1
+                sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+                wait(250)
+                downloadUrlToFile(updatelink, thisScript().path,
+                  function(id3, status1, p13, p23)
+                    if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                      print(string.format('Загружено %d из %d.', p13, p23))
+                    elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                      print('Загрузка обновления завершена.')
+                      sampAddChatMessage((prefix..'Обновление завершено!'), color)
+                      goupdatestatus = true
+                      lua_thread.create(function() wait(500) thisScript():reload() end)
+                    end
+                    if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                      if goupdatestatus == nil then
+                        sampAddChatMessage((prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+                        update = false
+                      end
+                    end
+                  end
+                )
+                end, prefix
+              )
+            else
+              update = false
+              print('v'..thisScript().version..': Обновление не требуется.')
             end
+          end
+        else
+          print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
+          update = false
         end
-    end)
+      end
+    end
+  )
+  while update ~= false do wait(100) end
 end
